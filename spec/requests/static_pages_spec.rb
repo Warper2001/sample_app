02cf570a06a595bec4ea722bd83f5a819dde3ed1
Alex Bernstein <warper2001@gmail.com>
2012-12-18 10:41:33 -0600

    3.3.2 Passing title tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,15 +7,20 @@ describe "StaticPages" do
     it "should have the content 'Sample App" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1',:text=>"Sample App")
     end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',:text=>"Ruby on Rails Tutorial Sample App | Home")
   end
+end
 
   describe "Help page" do
 
   	it "should have the content 'Help'" do
   		visit '/static_pages/help' 
-  		page.should have_content('Help')
+  		 page.should have_selector('h1',:text=>"Help")
+       page.should have_selector('title',:text=>"Ruby on Rails Tutorial Sample App | Help")      
   	end
   end
 
@@ -23,7 +28,8 @@ describe "StaticPages" do
 
     it "should have the content 'About Us" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1',:text=>"About Us")
+      page.should have_selector('title',:text=>"Ruby on Rails Tutorial Sample App | About Us")      
     end
   end
 end
