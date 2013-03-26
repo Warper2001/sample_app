fe0124ae6b42ec5c69040b67a233f54465e945ae
Alex Bernstein <warper2001@gmail.com>
2013-03-26 16:22:28 -0500

    8.2.2 A working sign_in method

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -31,7 +31,7 @@ describe "User pages" do
 				before { click_button submit }
 
 				it { should have_selector('title', text: 'Sign up') }
-				it { should have_content('error') }
+    		    it { should have_content('error') }
 			end
 		end
 
@@ -53,6 +53,7 @@ describe "User pages" do
 
 				it { should have_selector('title', text: user.name) }
 				it { should have_selector('div.alert.alert-sucess', text: 'Welcome') }
+			end
 		end
 	end
 end
