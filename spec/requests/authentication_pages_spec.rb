46ff44248ba39e1458140ff0656aeca449ca5a21
Alex Bernstein <warper2001@gmail.com>
2013-03-28 18:07:22 -0500

    Revert "pass bug"
    This reverts commit ff2e304322d09dbc72143a2feab8320882dc2194.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -28,7 +28,7 @@ describe "Authentication" do
   		let(:user) { FactoryGirl.create(:user) }
   		before do
   			fill_in "Email", 	with: user.email.upcase
-  			fill_in "Password", with: user.password
+  			fill_in "Password", with: user.Password
   			click_button "Sign in"
   		end
 
