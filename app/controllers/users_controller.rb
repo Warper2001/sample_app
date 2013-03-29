31905cf0d3b45ad40a7a96977a9cc75bfec501cb
Alex Bernstein <warper2001@gmail.com>
2013-03-29 16:57:14 -0500

    9.3.3 Pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_filter :signed_in_user, only:[:index, :edit, :update]
+	before_filter :correct_user, only:[:edit, :update]
 	def show
 		@user = User.find(params[:id])
 	end
@@ -15,11 +17,12 @@ class UsersController < ApplicationController
 			render 'new'
 		end
 	end
+	def index
+		@users = User.all
+	end
 	def edit
-		@user= User.find(params[:id])
 	end
 	def update
-		@user = User.find(params[:id])
 		if @user.update_attributes(params[:user])
 			flash[:success] = "Profile updated"
 			sign_in @user
@@ -27,5 +30,19 @@ class UsersController < ApplicationController
 		else
 			render 'edit'
 		end
+	end
+
+	private
+
+	def signed_in_user
+		unless signed_in?
+			store_location
+			redirect_to signin_url, notice: "Please sign in."
+		end
+	end
+
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_path) unless current_user?(@user)
 	end
 end
